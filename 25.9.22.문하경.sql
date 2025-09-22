a272c12fa306fe25ed39850de17741a0af7bc28e
haha0103 <hinalinahili2008@gmail.com>
2025-09-22 15:30:23 +0900

    Add files via upload

--- 25.9.22.문하경.sql
+++ 25.9.22.문하경.sql
@@ -41,22 +41,29 @@ SELECT s.name "STU_NAME",  p.name "PROF_NAME" from student s FULL OUTER JOIN pro
 
 --4장 JOIN 연습문제
 --1. 학생 테이블 (student) 과 학과 테이블 (department) 테이블을 사용하여 학생이름, 1 전공학과번호(deptno1) , 1전공 학과 이름을 출력하세요.
- 
- 
- 
---2. emp2 테이블과 p_grade 테이블을 조회하여 현재 직급이 있는 사원의 이름과 직급, 현재 연봉, 해당 직급의 연봉의 하한금액과 상한 금액을 아래 결과 화면과 같이 출력하세요.
+SELECT * FROM student;
+SELECT * from department;
+SELECT s.name "STU_NAME", s.DEPTNO1, d.dname "DEPT_NAME" from student s, department d
+where s.deptno1 = d.deptno;
 
+--2. emp2 테이블과 p_grade 테이블을 조회하여 현재 직급이 있는 사원의 이름과 직급, 현재 연봉, 해당 직급의 연봉의 하한금액과 상한 금액을 아래 결과 화면과 같이 출력하세요.
+SELECT name, e2.position, pay, s_pay low_pay, e_pay high_pay from emp2 e2, p_grade p where e2.position = p.position;
 
 
    
 --3. Emp2 테이블과 p_grade 테이블을 조회하여 사원들의 이름과 나이, 현재 직급 , 예상 직급 을 출력하세요. 
 --예상 직급은 나이로 계산하며 해당 나이가 받아야 하는 직급을 의미합니다.  나이는 오늘(sysdate)을 기준으로 하되 trunc 로 소수점 이하는 절삭해서 계산하세요.
-
-
+SELECT * from emp2;
+SELECT * from p_grade;
+SELECT name, trunc(months_between(sysdate,e2.birthday)/12) as "age", e2.position as "curr_position", p.position
+from emp2 e2, p_grade p where trunc(months_between(sysdate,e2.birthday)/12) BETWEEN p.s_age and p.e_age;
 
 --4 . customer 테이블과 gift 테이블을 Join하여 고객이 자기 포인트보다 낮은 포인트의 상품 중 한가지를 선택할 수 있다고 할 때,
 --Notebook 을 선택할 수 있는 고객명과 포인트, 상품명을 출력하세요.
-
+SELECT * from customer;
+SELECT * from gift;
+SELECT c.gname, c.point, g.gname from customer c, gift g
+where g.g_start <= c.point and g.gname = 'Notebook';
 
 --5. professor 테이블에서 교수의 번호, 교수이름, 입사일, 자신보다 입사일 빠른 사람 인원수를 출력하세요. 단 자신보다 입사일이 빠른 사람수를 오름차순으로 출력하세요.  
 
